77f261ceb54b62f33aec422ab9fc23f8ddd0ad0d
Hriday Bhambhvani <hridaybhambhvani@DN52eeq6.SUNet>
2020-03-16 11:56:28 -0700

    latest

--- evalTest.py
+++ evalTest.py
@@ -11,6 +11,7 @@ import torchvision
 import pdb
 from sklearn.metrics import roc_curve, auc
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics._plot.confusion_matrix import ConfusionMatrixDisplay
 
 
 test_loader = torch.utils.data.DataLoader(
@@ -52,14 +53,14 @@ def eval(): #evaluates test set
 		roc_auc = dict()
 		outs = torch.Tensor([]).to(device)
 		labels = torch.Tensor([]).long().to(device)
-		preds = torch.Tensor([]).to(device)
+		preds = torch.Tensor([]).long().to(device)
 		for q, batch in enumerate(test_loader, 1):
 			data, label = batch[0].to(device), batch[1].to(device)
 			out = F.softmax(model(data), dim=-1)
 			outs = torch.cat((outs, out), dim = 0)
 			labels = torch.cat((labels, label), dim = 0)
 			_, pred = out.max(-1)
-			preds = torch.cat((preds, ped), dim = 0)
+			preds = torch.cat((preds, pred), dim = 0)
 			acc = pred.eq(label).float().mean().item()
 			acc_sum += acc*data.size(0)
 			n_sum += data.size(0)
@@ -75,15 +76,12 @@ def eval(): #evaluates test set
 	return labels, preds
 
 labels, preds = eval()
-pdb.set_trace()
 
-print(confusion_matrix(labels, preds, labels = ["1", "2", "3"]))
+cm = confusion_matrix(labels.cpu().numpy(), preds.cpu().numpy(), labels = [0, 1, 2])
+display_labels = ["MES 1", "MES 2", "MES 3"]
+#plot_confusion_matrix(cm, plot_labels, title = "Confusion Matrix")
 
-
-
-
-
-
+disp = ConfusionMatrixDisplay(confusion_matrix = cm, display_labels = display_labels)
 
 
 
